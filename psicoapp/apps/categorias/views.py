0d66a11cb4983a31dd092572a699384971f3aa06
Inveninatonicolas <nicolasdossinveninato@gmail.com>
2025-06-13 13:46:49 -0300

    [categoria-views] Implementação das views e formulário da Categoria

--- psicoapp/apps/categorias/views.py
+++ psicoapp/apps/categorias/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Categoria
+from .forms import CategoriaForm
 
 # Create your views here.
+
+def listar_categorias(request):
+    categorias = Categoria.objects.all()
+    return render(request, 'categorias/list.html', {'categorias': categorias})
+
+def criar_categoria(request):
+    form = CategoriaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('categorias:listar')
+    return render(request, 'categorias/form.html', {'form': form, 'titulo': 'Nova Categoria'})
+
+def editar_categoria(request, id_categoria):
+    categoria = get_object_or_404(Categoria, id=id_categoria)
+    form = CategoriaForm(request.POST or None, instance=categoria)
+    if form.is_valid():
+        form.save()
+        return redirect('categorias:listar')
+    return render(request, 'categorias/form.html', {'form': form, 'titulo': 'Editar Categoria'})
+
+def excluir_categoria(request, id_categoria):
+    categoria = get_object_or_404(Categoria, id=id_categoria)
+    if request.method == 'POST':
+        categoria.delete()
+        return redirect('categorias:listar')
+    return render(request, 'categorias/confirmar_exclusao.html', {'categoria': categoria})
