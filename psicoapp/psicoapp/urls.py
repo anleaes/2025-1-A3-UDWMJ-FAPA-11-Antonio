dc7f0b31ec4f7861ebf15079af4c7149614355d3
Inveninatonicolas <nicolasdossinveninato@gmail.com>
2025-06-23 23:45:57 -0300

    implementação das urls de medicamento itens

--- psicoapp/psicoapp/urls.py
+++ psicoapp/psicoapp/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('medicamentos/', include('apps.medicamentos.urls', namespace='medicamentos')),
     path('prescricoes/', include('apps.prescricoes.urls', namespace='prescricoes')),
     path('prontuarios/', include('apps.prontuarios.urls', namespace='prontuarios')),
+    path('medicamentoitens/', include('apps.medicamentoitens.urls', namespace='medicamentoitens')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
